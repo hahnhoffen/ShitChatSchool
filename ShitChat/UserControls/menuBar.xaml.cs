086499c565631c5cf180bfe46a59c1e26dfeacb6
Bubbelbad <victor.ivarson@hotmail.se>
2023-11-30 13:44:13 +0100

    new menuBar

--- ShitChat/UserControls/menuBar.xaml.cs
+++ ShitChat/UserControls/menuBar.xaml.cs
@@ -12,20 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ShitChat.UserControls;
 
-namespace ShitChat
+namespace ShitChat.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for menuBar.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class menuBar : UserControl
     {
-        public MainWindow()
+        public menuBar()
         {
-            InitializeComponent();           
-            Login login = new Login();
-            login.Show();
+            InitializeComponent();
         }
     }
 }
