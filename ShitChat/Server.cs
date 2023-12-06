8e7d998b8ef0281b87f705b9fd7935fffc8d6798
RaashidM <raashidm24@gmail.com>
2023-12-06 15:15:21 +0100

    Commit

--- ShitChat/Server.cs
+++ ShitChat/Server.cs
@@ -11,44 +11,6 @@ namespace ShitChat
 {
     public class Server
     {
-        private TcpListener tcpListener;
-        private Thread listenerThread;
-        private List<ClientHandler> connectedClients = new List<ClientHandler>();
-
-        public Server()
-        {
-            this.tcpListener = new TcpListener(IPAddress.Any, 5000);
-            this.listenerThread = new Thread(new ThreadStart(ListenForClients));
-            this.listenerThread.Start();
-        }
-
-        private void ListenForClients()
-        {
-            this.tcpListener.Start();
-
-            while (true)
-            {
-                TcpClient client = this.tcpListener.AcceptTcpClient();
-                ClientHandler clientHandler = new ClientHandler(client, this);
-                connectedClients.Add(clientHandler);
-
-                Thread clientThread = new Thread(new ThreadStart(clientHandler.HandleClientComm));
-                clientThread.Start();
-            }
-        }
-
-        public void BroadcastMessage(string message, ClientHandler sender)
-        {
-            foreach (ClientHandler client in connectedClients)
-            {
-                if (client != sender) // Skicka inte tillbaka meddelandet till avsändaren
-                    client.SendMessage(message);
-            }
-        }
-
-        public void RemoveClient(ClientHandler client)
-        {
-            connectedClients.Remove(client);
-        }
+       
     }
 }
