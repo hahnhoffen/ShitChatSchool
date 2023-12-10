9ba1607a8806add0fbe8ad9c60352171f28aa4dd
Bubbelbad <victor.ivarson@hotmail.se>
2023-12-10 14:47:55 +0100

    new select for avatarMenu

--- ShitChat/UserControls/avatarMenu.xaml.cs
+++ ShitChat/UserControls/avatarMenu.xaml.cs
@@ -20,14 +20,146 @@ namespace ShitChat.UserControls
     /// </summary>
     public partial class avatarMenu : UserControl
     {
+
+        int picked;
+
         public avatarMenu()
         {
             InitializeComponent();
         }
 
+
         private void pickButton_Click(object sender, RoutedEventArgs e)
         {
             avatarMenuClass.Visibility = Visibility.Collapsed;
+            Pick();
+        }
+
+
+        public void Pick()
+        {
+            switch (picked)
+            {
+                case 0:
+                    break;
+                case 1:
+                    border1.Opacity = 0.4;
+                    break;
+                case 2:
+                    border2.Opacity = 0.4;
+                    break;
+                case 3:
+                    border3.Opacity = 0.4;
+                    break;
+                case 4:
+                    border4.Opacity = 0.4;
+                    break;
+                case 5:
+                    border5.Opacity = 0.4;
+                    break;
+                case 6:
+                    border6.Opacity = 0.4;
+                    break;
+            }
+        }
+
+
+        private void profileBtn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (border1.Opacity == 0.4)
+            {
+                border1.Opacity = 0.8;
+                picked = 1;
+            }
+            else
+            {
+                border1.Opacity = 0.4;
+                picked = 0;
+            }
+        }
+
+
+        private void profileBtn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (border2.Opacity == 0.4)
+            {
+                border2.Opacity = 0.8;
+                picked = 2;
+            }
+            else
+            {
+                border2.Opacity = 0.4;
+                picked = 0;
+            }
+        }
+
+        private void profileBtn3_Click(object sender, RoutedEventArgs e)
+        {
+            if (border3.Opacity == 0.4)
+            {
+                border3.Opacity = 0.8;
+                picked = 3;
+            }
+            else
+            {
+                border3.Opacity = 0.4;
+                picked = 0;
+            }
+        }
+
+        private void profileBtn4_Click(object sender, RoutedEventArgs e)
+        {
+            if (border4.Opacity == 0.4)
+            {
+                border4.Opacity = 0.8;
+                picked = 4;
+            }
+            else
+            {
+                border4.Opacity = 0.4;
+                picked = 0;
+            }
+        }
+
+        private void profileBtn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (border5.Opacity == 0.4)
+            {
+                border5.Opacity = 0.8;
+                picked = 5;
+            }
+            else
+            {
+                border5.Opacity = 0.4;
+                picked = 0;
+            }
+        }
+
+        private void profileBtn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (border6.Opacity == 0.4)
+            {
+                border6.Opacity = 0.8;
+                picked = 6;
+            }
+            else
+            {
+                border6.Opacity = 0.4;
+                picked = 0;
+            }
+        }
+
+        public void ButtonCheck(Button button)
+        {
+            if (button.Opacity == 0.4)
+            {
+                button.Opacity = 0.8;
+            }
+            else
+            {
+                button.Opacity = 0.4;
+                picked = 0;
+            }
         }
     }
 }
