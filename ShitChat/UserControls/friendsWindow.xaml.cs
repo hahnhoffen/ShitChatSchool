cde8c2478c6f2dabf1d573db17cd46851d014918
Frooddie <freddie.andreasson@student.nbi.handelsakademin.se>
2023-12-15 12:08:01 +0100

    12:07

--- ShitChat/UserControls/friendsWindow.xaml.cs
+++ ShitChat/UserControls/friendsWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,118 +24,113 @@ namespace ShitChat.UserControls
     /// </summary>
     public partial class friendsWindow : UserControl
     {
-
+        public List<User> Users { get; private set; }
         UserManager manager = new UserManager();
         User user = new User();
+
         public friendsWindow()
         {
-       //     textboxAddFriend.Visibility = Visibility.Hidden;
-       //     searchNewFriends.Visibility = Visibility.Hidden;
-       //     removeFriend_ListBox.Visibility = Visibility.Hidden;
-       //     AddFriend_ListBox.Visibility = Visibility.Hidden;
-       //     AddFriend_ListBox.IsEnabled = false;
-       //     removeFriend_ListBox.IsEnabled = false;
             InitializeComponent();
+            LoadUserData();
+            DataContext = this;
         }
-
-
+        private void LoadUserData()
+        {
+            string jsonF = manager.usersPath;
+            try
+            {
+                string jsonContent = File.ReadAllText(jsonF);
+                Users = JsonConvert.DeserializeObject<List<User>>(jsonContent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
         private void AddFriends_Click(object sender, RoutedEventArgs e)
         {
-            AddFriends.Visibility = Visibility.Hidden;
-            RemoveFriends.Visibility = Visibility.Hidden;
-            textboxAddFriend.Visibility = Visibility.Visible;
+            textHelper.Visibility = Visibility.Visible;
             searchNewFriends.Visibility = Visibility.Visible;
+            search_Button.Visibility = Visibility.Visible;
             AddFriend_ListBox.Visibility = Visibility.Visible;
-            AddFriend_ListBox.IsEnabled = true;
         }
-
-
         private void RemoveFriends_Click(object sender, RoutedEventArgs e)
         {
-            AddFriends.Visibility = Visibility.Hidden;
-            RemoveFriends.Visibility = Visibility.Hidden;
-            textboxAddFriend.Visibility = Visibility.Hidden;
-            searchNewFriends.Visibility = Visibility.Hidden;
-            AddFriend_ListBox.Visibility = Visibility.Hidden;
-            removeFriend_ListBox.Visibility = Visibility.Visible;
-            removeFriend_ListBox.IsEnabled = true;
+            if (removeFriend_ListBox.Visibility != Visibility.Visible)
+            {
+                removeFriend_ListBox.Visibility = Visibility.Visible;
+                if (removeFriend_ListBox.SelectedItem != null)
+                {
+                    string message = "Are you sure you want to remove this friend?";
+                    string title = "Remove friend";
+                    var result = MessageBox.Show(message, title, MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        manager.currentUser.RemoveFriend(user);
+                        MessageBox.Show("User have been removed as your friend");
+                    }
+                    //else if (removeFriend_ListBox == null)
+                    //{
+                    //    MessageBox.Show("Sadly you have no friends..");
+                    //}
+                    //else if (removeFriend_ListBox.Visibility == Visibility.Hidden)
+                    //{
+                    //    return;
+                    //}
+                }
+            }
         }
-
-
         private void search_Button_Click(object sender, RoutedEventArgs e)
         {
-            string searching = searchNewFriends.Text;
-            if (searchNewFriends.Text != "")
+            string searching = searchNewFriends.Text.Trim();
+            if (!string.IsNullOrEmpty(searching))
             {
-                if (searchNewFriends.Text == user.UserName) 
+                User foundUser = Users.FirstOrDefault(user => user.UserName == searching);
+                if (foundUser != null)
                 {
-                    foreach (var apllication in user.UserName)
+                    AddFriend_ListBox.Items.Add(foundUser.UserName);
+                    string message = $"Do you want to add {foundUser.UserName} to your friendlist?";
+                    string title = "Add friend";
+                    var result = MessageBox.Show(message, title, MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.Yes)
                     {
-                        AddFriend_ListBox.Items.Add(searching);
-                        if (AddFriend_ListBox.SelectedItem != null)
-                        {
-                            string message = "Do you want to add this user to your friendlist?";
-                            string title = "Add friend";
-                            var result = MessageBox.Show(message, title, MessageBoxButton.YesNo);
-                            if (result == MessageBoxResult.Yes)
-                            {
-                                manager.currentUser.AddFriend(user);
-                                MessageBox.Show("User have been added as your friend");
-                            }
-                        }
+                        user.AddFriend(user);
+                        MessageBox.Show($"{foundUser.UserName} have been added as your friend");
+                        AddFriend_ListBox.Items.Clear();
                     }
                 }
-            }
-            else 
-            {
-                MessageBox.Show("User not found, please try again!");
+                else
+                {
+                    MessageBox.Show("User not Found, please try again!");
+                }
             }
         }
-
-
-        private void removeFriend_ListBox_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        public void ShowFriendsWindow()
         {
-            if (removeFriend_ListBox.Visibility != Visibility.Hidden)
+            this.Visibility = Visibility.Visible;
+        }
+        public void HideFriendsWindow()
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+        /*private void removeFriend_ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (removeFriend_ListBox.Visibility != Visibility.Visible)
             {
-                while (removeFriend_ListBox.IsEnabled == true)
+                removeFriend_ListBox.Visibility = Visibility.Visible;
+                if (manager.currentUser.friendsList.Contains(user))
                 {
-                    foreach (var friend in manager.currentUser.friendsList)
+                    foreach (User friend in manager.currentUser.friendsList)
                     {
                         removeFriend_ListBox.Items.Add(friend);
-                        Console.WriteLine(manager.currentUser.friendsList);
-                    }
-                    if (removeFriend_ListBox.SelectedItem != null)
-                    {
-                        string message = "Are you sure you want to remove this friend?";
-                        string title = "Remove friend";
-                        var result = MessageBox.Show(message, title, MessageBoxButton.YesNo);
-                        if (result == MessageBoxResult.Yes)
-                        {
-                            manager.currentUser.RemoveFriend(user);
-                            MessageBox.Show("User have been removed as your friend");
-                        }
+                        //Console.WriteLine(manager.currentUser.friendsList);
                     }
                 }
-            }
-        }
-
-
-        private void AddFriend_ListBox_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (AddFriend_ListBox.Visibility == Visibility.Visible)
-            {
-                AddFriend_ListBox.Items.Clear();
-                while (AddFriend_ListBox.IsEnabled == true)
+                else
                 {
-                    if (searchNewFriends.Text != null)
-                    {
-                        if (searchNewFriends.Text == user.UserName)
-                        {
-                            AddFriend_ListBox.Items.Add(user.UserName);
-                        }
-                    }
+                    MessageBox.Show("Sadly you have no friends..");
                 }
             }
-        }
+        }*/
     }
 }
